d5ea51ddc8d8db1a5e31809de3814c9c4147160b
Артем Шафранівський <Shafranivskyi.A.O@nmu.one>
2023-07-31 18:41:46 +0300

    Create a foundation for a future web application and adding main models to folder "Core" in "Courses.Domain" class library

---
+++ Courses.Domain/Courses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain
+{
+    public  class Courses
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public decimal price { get; set; }
+        public DateTime Expires { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string FilePath { get; set; }
+        public ICollection<Images> Images { get; set; }
+        public int Rating { get; set; }
+        public ICollection<Comments> Comments { get; set; }
+
+        public Courses() => 
+            Images = new List<Images>();
+            Comments = new List<Images>();
+    }
+}
---
+++ Courses.Domain/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Domain
+{
+    public class Messages
+    {
+        public Guid Id { get; set; }
+        public AppUser AppUser { get; set; }
+        public DateTime Sended { get; set; }
+        public string Text { get; set; }
+        public Chats Chat { get; set; }
+        public ICollection<Images> Images { get; set; }
+        public Messages()=>
+            Images = new List<Images>();
+    }
+}
