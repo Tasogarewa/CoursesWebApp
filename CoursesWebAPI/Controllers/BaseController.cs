a8765733611ae7cea774e6c382eca86188f93d1c
Артем Шафранівський <Shafranivskyi.A.O@nmu.one>
2023-08-03 00:23:09 +0300

    A little configuring web api and create controller base class

---
+++ CoursesWebAPI/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CoursesWebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator Mediator;
+        protected IMediator mediator => Mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        internal Guid UserId => !User.Identity.IsAuthenticated
+                ? Guid.Empty
+                : Guid.Parse(AppUser.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
